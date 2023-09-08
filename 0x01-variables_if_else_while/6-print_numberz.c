32278cdc49b0e77a4525b27b1c1ea40cfa229047
Eunicekahinga <euniwahito@gmail.com>
2023-09-08 14:51:26 -0700

    Printing single digit integers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers from 0 to 9
+  * main - Prints the single digits between 0 and 10
   *
   * Return: Always (Success)
   */
 int main(void)
 {
-	char y;
+	int y;
 
-	for (y = '0'; y <= '9'; y++)
+	for (y = '0'; y	<= '9'; y++)
 	{
 		putchar(y);
 	}
