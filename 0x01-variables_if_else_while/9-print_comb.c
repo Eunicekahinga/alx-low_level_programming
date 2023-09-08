af3dbcce941b44a9d424ae99c17544a850abf9de
Eunicekahinga <euniwahito@gmail.com>
2023-09-08 15:10:01 -0700

    Pinting numbers with spaces and comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - Printing numbers separated with comma and space using putchar
+  *
+  * Return: Always (Success);
+  */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
