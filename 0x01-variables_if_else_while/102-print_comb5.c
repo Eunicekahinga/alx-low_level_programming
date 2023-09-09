254908ec9e5291b33896644fe7f1cbd5bfeeb0d7
Eunicekahinga <euniwahito@gmail.com>
2023-09-08 18:08:30 -0700

    correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,33 +7,29 @@
   */
 int main(void)
 {
-	int c, j, i;
+        int c, i, j;
 
-	c = j = i = '0';
+        c = j = i = '0';
 
-	for (c = '0'; c <= '9'; c++);
-	{
-		for (i = '0'; i <= '9'; i++);
-		{
-			putchar(c);
-			putchar(i);
-			
-			if (i > '0' && c > '0');
-			{
-				putchar(' ');
-				putchar(j);
-				putchar(j);
+        for (c = '0'; c <= '9'; c++)
+        {
+                for (i = '0'; i <= '9'; i++)
+                {
+                        putchar(c);
+                        putchar(i);
+                        putchar(' ');
+                        putchar(j);
+                        putchar(j);
 
-				if ((c != '9') || (c == '9' && i != '9'));
-				{	
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
+                        if ((c != '9') || (c == '9' && i != '9'))
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+        }
 
-	putchar('\n');
+        putchar('\n');
 
-	return (0);
+        return (0);
 }
