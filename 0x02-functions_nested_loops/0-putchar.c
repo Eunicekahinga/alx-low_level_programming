18bf2b25fa7b2cafe77ec0c9b7feb9f90c01143f
Eunicekahinga <euniwahito@gmail.com>
2023-09-12 14:23:59 -0700

    Printing output in c using printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main prints '_putchar'
+  *The function prints _putchar
+  *
+  *
+  *Return: Always (Success)
+  */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
