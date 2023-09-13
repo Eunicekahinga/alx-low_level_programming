42b1e4a0573e58197ce303267f5ec23f6622212f
Eunicekahinga <euniwahito@gmail.com>
2023-09-13 14:17:33 -0700

    Checking whether a number is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - prints whether a number is positive or negative
+ *
+ *positive_or_negative - it checks whether i is
+ *positive or negative
+ *Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+A readme file for tasks on debugging
