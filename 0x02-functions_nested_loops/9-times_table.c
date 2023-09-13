9739eb552d7c22f8356bd05057cd92b8a9683415
Eunicekahinga <euniwahito@gmail.com>
2023-09-12 19:51:31 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "main.n"
+/**
+ * times_table - Check description for the type
+ * Description: Prints 9 times the time table
+ * Return: No return
+ */
+void times_table(void)
+{
+	int i, j, n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
