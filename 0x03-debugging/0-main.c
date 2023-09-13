43d90a75b132849d13ba31908249f98627fb5367
Eunicekahinga <euniwahito@gmail.com>
2023-09-13 14:39:38 -0700

    testing the output

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,6 @@
 /**
  *main - tests function that prints if integer is positive or negative
  *Return: 0
- *
  */
 
 int main(void)
@@ -14,5 +13,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }
