d0901f026d9e2108f3ce40646e8ea38f162f7172
Eunicekahinga <euniwahito@gmail.com>
2023-09-12 20:04:12 -0700

    printing values

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-  * print_to_98 - Prints all natural numbers from n to 98
+  * print_to_98 - Prints all the numbers from n to 98
   * @n: The number to start printing from
   *
   * Return: Always 0.
