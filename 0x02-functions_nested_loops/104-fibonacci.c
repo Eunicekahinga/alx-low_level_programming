51089342edab8d024e94d51da2cd029f1d9c8701
Eunicekahinga <euniwahito@gmail.com>
2023-09-13 08:23:54 -0700

    Printing fibonacci to 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the first 98 fibonacci numbers
-  *
-  * Return: No return
-  */
+ * main - Prints the first 98 Fibonacci numbers using custom arithmetic
+ *
+ * Return: No return
+ */
 int main(void)
 {
-	int i = 0;
+	int i;
 	unsigned long j = 1, k = 2;
+	unsigned long carry = 0;
 
-	while (i < 98)
+	printf("%lu, %lu", j, k);
+
+	for (i = 2; i < 98; i++)
 	{
-		if (i == 0)
-			printf("%lu", j);
-		else if (i == 1)
-			printf(", %lu", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %lu", k);
-		}
+		unsigned long temp = j + k + carry;
 
-		i++;
+	if (temp >= 10000000000UL)
+	{
+		carry = temp / 10000000000UL;
+		temp %= 10000000000UL;
+	}
+	else
+	{
+		carry = 0;
+	}
+
+	j = k;
+	k = temp;
+
+	printf(", %010lu", k);
 	}
 
 	printf("\n");
+
 	return (0);
 }
