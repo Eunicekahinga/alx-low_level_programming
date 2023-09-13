a9df7d01681f7918a30782f5edd0eb78f607b829
Eunicekahinga <euniwahito@gmail.com>
2023-09-12 19:33:32 -0700

    last value  digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_last_digit - Prints the last digit of a number
+  * @n: The number to be checked
+  *
+  * Return:  The last value of a digit number
+  */
+int print_last_digit(int k)
+{
+	int digit;
+
+	digit = k % 10;
+
+	if (digit < 0)
+	{
+		digit = digit * -1;
+	}
+
+	_putchar(digit + '0');
+
+	return (digit);
+}
