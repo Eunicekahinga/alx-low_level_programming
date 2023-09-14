946e0be39ecedb72c9a38b62fa568694a98237ac
Eunicekahinga <euniwahito@gmail.com>
2023-09-14 07:58:18 -0700

    Printing numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0-9
+ * followed by a line
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
