d7791e6a601ef59daaab1dc85abfbd87b113f7bf
Eunicekahinga <euniwahito@gmail.com>
2023-09-18 14:45:41 -0700

    Password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ * generatepassword - generates random password
+ * for a program
+ * @password: the password to be generated
+ * @length: the length of password generated
+ * Return: void
+ */
+
+void generatepassword(char *password, int length)
+{
+	char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	int charsetLength = sizeof(charset) - 1;
+
+	srand(time(NULL));
+
+	for (int i = 0; i < length; i++)
+	{
+		int index = rand() % charsetLength;
+
+		password[i] = charset[index];
+	}
+
+	password[length] = '\0';
+}
+
+int main(void)
+{
+	int numberofPasswords = 6;
+	int passwordLength = 8;
+	int i;
+
+	for (i = 0; i < numberofPasswords; i++)
+	{
+		char password[passwordLength + 1];
+
+		generatepassword(password, passwordLength);
+
+		printf("%s\n", password);
+	}
+	return (0);
+}
