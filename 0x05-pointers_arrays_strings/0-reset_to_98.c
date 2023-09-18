dfb0d620e6f5e8cc0af02b44ee60d9283b119d28
Eunicekahinga <euniwahito@gmail.com>
2023-09-18 02:05:04 -0700

    Using pointers to update values of variables

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * resert_to_98 - Updates the value of an int to 98
+ *@n: the pointer to the integer
+ *
+ * Return: success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
