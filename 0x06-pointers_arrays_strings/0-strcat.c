303ff1cb651293410c5af83bf3c6075349e34943
Eunicekahinga <euniwahito@gmail.com>
2023-09-20 08:38:27 -0700

    concatenating a string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,15 +10,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int i, length = 0;
+	int i, length = 0;
 
-        for (i = 0; dest[i] != '\0'; i++)
-        {
-                length++;
-        }
-        for (i = 0; src[i] != '\0'; i++)
-        {
-                dest[length + i] = src[i];
-        }
-        return (dest);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		length++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[length + i] = src[i];
+	}
+	return (dest);
 }
