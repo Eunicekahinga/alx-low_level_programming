7d4c368fb60f5c11b746e5eb0ed44b237c1e1992
Eunicekahinga <euniwahito@gmail.com>
2023-09-21 05:11:03 -0700

    Rot13 encryption

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,15 +12,18 @@ char *rot13(char *p)
 {
 	int i = 0;
 
-	while (p[i] != '\0')
+	while (p[i])
 	{
 		while ((p[i] >= 'a' && p[i] <= 'z') || (p[i] >= 'A' && p[i] <= 'Z'))
 		{
 			if ((p[i] >= 'a' && p[i] <= 'm') || (p[i] >= 'A' && p[i] <= 'M'))
+			{
 				p[i] += 13;
-			else
+				break;
+			}
+
 				p[i] -= 13;
-			i++;
+				break;
 		}
 		i++;
 	}
