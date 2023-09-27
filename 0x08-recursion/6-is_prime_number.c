41de3b5a7d9ef4c6d6c561b05ba1986948aa0920
Eunicekahinga <euniwahito@gmail.com>
2023-09-27 10:26:28 -0700

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * is_prime_number - the function checks whether a
+ * is_prime_recursive - the function checks whether a
  * number is prime or not
  * @n: the number
+ * @k: the number used to divide
  * Return: 1 if its a prime number otherwise
  * return 0
  *
  */
 
-int is_prime_number(int n)
+int is_prime_recursive(int n, int k)
 {
-	int k;
-
 	if (n <= 1)
 	{
 		return (0);
@@ -22,18 +21,23 @@ int is_prime_number(int n)
 		return (1);
 	}
 
-	if (n % 2 == 0)
+	if (k == 1)
+	{
+		return (1);
+	}
+
+	if (n % k == 0)
 	{
 		return (0);
 	}
 
-	for (k = 3; k * k <= n; k += 2)
-	{
-		if (n % k == 0)
-		{
-			return (0);
-		}
-	}
+	return (is_prime_recursive(n, k - 1));
+}
+/**
+ * is_prime_number - checks whether number is prime
+ */
 
-	return (1);
+int is_prime_number(int n)
+{
+	return (is_prime_recursive(n, n - 1));
 }
