cdaf350840b9a441564405b64efb558753934006
Eunicekahinga <euniwahito@gmail.com>
2023-09-27 06:35:20 -0700

    Printing string length and factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - it prints factorial of a number
+ * @n: the number
+ * Return: the factorial,
+ * 1 if 0 and -1 if less than 0
+ *
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	return n * factorial(n - 1);
+}
