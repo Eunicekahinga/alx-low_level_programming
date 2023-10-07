b9c7420e2b0101cf752d778db618bdfe467c7473
Eunicekahinga <euniwahito@gmail.com>
2023-10-07 04:21:31 -0700

    Reallocating memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,7 +23,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (ptr == NULL)
-		return (malloc(new_size));
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
 
 	if (new_size == old_size)
 		return (ptr);
