61a6e5490c136308ecde404bd29933b118477467
Eunicekahinga <euniwahito@gmail.com>
2023-10-09 16:39:10 -0700

    creating a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,29 +14,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
+	char *nameCopy;
+	char *ownerCopy;
 
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
 
-	newDog = (dog_t *)malloc(sizeof(dog_t));
+	newDog = malloc(sizeof(dog_t));
 
 	if (newDog == NULL)
 		return (NULL);
 
-	newDog->name = strdup(name);
-	newDog->owner = strdup(owner);
+	nameCopy = strdup(name);
+	ownerCopy = strdup(owner);
 
-	if (newDog->name == NULL || newDog->owner == NULL)
+	if (nameCopy == NULL || ownerCopy == NULL)
 	{
-		free(newDog->name);
-		free(newDog->owner);
+		free(nameCopy);
+		free(ownerCopy);
 		free(newDog);
 		return (NULL);
 	}
 
+	newDog->name = nameCopy;
 	newDog->age = age;
+	newDog->owner = ownerCopy;
 
 	return (newDog);
 }
