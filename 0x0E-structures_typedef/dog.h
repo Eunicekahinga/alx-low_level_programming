fe03246c77c8e0ee9a9df3fdeb7d148e9ccfaa19
Eunicekahinga <euniwahito@gmail.com>
2023-10-09 15:05:07 -0700

    Creating a new type of elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog - Defines three elements
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ *
+ * Description: three elements of struct dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+
+};
+#endif
