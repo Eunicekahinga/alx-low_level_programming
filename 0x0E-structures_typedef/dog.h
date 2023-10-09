6f429ac060ffca27f3bb80edf5ea6735462d8718
Eunicekahinga <euniwahito@gmail.com>
2023-10-09 15:30:09 -0700

    Initializing a typedef struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,11 +10,16 @@
  *
  * Description: three elements of struct dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
