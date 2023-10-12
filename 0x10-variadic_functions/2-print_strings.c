54daf16cec45ff5f4cf3c3421b37ff14ff418c73
Eunicekahinga <euniwahito@gmail.com>
2023-10-12 03:39:10 -0700

    printing a string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		stringVar = va_arg(args, char*);
 		if (stringVar == NULL)
-			printf("nil");
+			printf("(nil)");
 		else
 			printf("%s", stringVar);
 		if (separator == NULL)
