ffe957243370d4080b105bd00e4dac7eb1396f3f
Eunicekahinga <euniwahito@gmail.com>
2023-10-19 16:46:05 -0700

    Puting a function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+void  __attribute__((constructor)) firstPrinted(void);
+
+/**
+ * firstPrinted - function printed first
+ *
+ * Return: void
+ */
+void  __attribute__((constructor)) firstPrinted(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
