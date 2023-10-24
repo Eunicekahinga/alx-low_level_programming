8dfcb1e9b89f8ceba00800c565aef4b18e0e3108
Eunicekahinga <euniwahito@gmail.com>
2023-10-24 21:05:33 +0300

    Five

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *added;
 
-	if (*head == NULL)
+
+	if (*head == NULL || head == NULL)
 	{
 		return;
 	}
-	added = *head;
 
 	while (*head != NULL)
 	{
+		added = *head;
 		*head = (*head)->next;
 
 		free(added);
