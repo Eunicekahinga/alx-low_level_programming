65f92e1ec2f18076d71b337fcca3c4e21e8a0f56
Eunicekahinga <euniwahito@gmail.com>
2023-10-30 23:09:49 +0300

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,9 +16,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	file = open(filename, O_WRONLY | O_APPEND);
+
 	if (file == -1)
 		return (-1);
+
 	if (text_content == NULL)
 	{
 		close(file);
@@ -30,7 +33,13 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	appended = write(file, text_content, length);
 
-	if (close(file) == -1 || length != appended)
+	if (appended == -1 || length != appended)
+	{
+		close(file);
 		return (-1);
+	}
+	if (close(file) == -1)
+		return (-1);
+
 	return (1);
 }
