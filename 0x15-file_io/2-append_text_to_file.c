e864731ee86dd54af6b90f58b4848d5119f10055
Eunicekahinga <euniwahito@gmail.com>
2023-10-30 22:59:41 +0300

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 	if (text_content == NULL)
+	{
+		close(file);
 		return (1);
+	}
 
 	while (text_content[length] != '\0')
 		length++;
