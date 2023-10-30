fa60fd30492b4ad1cd3fc4015e7823303026f381
Eunicekahinga <euniwahito@gmail.com>
2023-10-31 00:39:13 +0300

    copying

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUF_SIZE 1024
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+/**
+ * main - a program to copy the contents of one file to another
+ * @argc: the number of command-line arguments
+ * @argv: an array of strings containing the command-line arguments
+ * Return: 0 on success, 97 on invalid arguments, 98 on read error,
+ * 99 on write error, 100 on close error
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, bytes_read, bytes_written;
+	char buffer[BUF_SIZE];
+	struct stat st;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		return (97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		return (99);
+	}
+	while ((bytes_read = read(file_from, buffer, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			return (99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		return (98);
+	}
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		return (100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		return (100);
+	}
+	if (fstat(file_to, &st) == 0)
+	{
+		if ((st.st_mode & PERMISSIONS) == PERMISSIONS)
+		{
+			return (0);
+		}
+	}
+	dprintf(STDERR_FILENO, "Error: Unable to change permissions\n");
+	return (99);
+}
